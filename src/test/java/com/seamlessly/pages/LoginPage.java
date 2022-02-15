b253196d59d54cf4a54bfdb7f2da3b02f1a8dc0c
Benjamin2955 <ozenbunyamin29@gmail.com>
2022-02-15 02:57:31 -0500

    LoginPage class is created

---
+++ src/test/java/com/seamlessly/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package com.seamlessly.pages;
+
+import com.seamlessly.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage(){  PageFactory.initElements(Driver.get(),this);}
+
+    @FindBy(id = "user")
+    public WebElement usernameBox;
+
+    @FindBy(id = "password")
+    public WebElement passwordBox;
+
+     @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+     @FindBy(id = "lost-password")
+    public WebElement forgotPassButton;
+
+     @FindBy(id = "reset-password-submit")
+    public WebElement resetPassButton;
+
+     @FindBy(css = "p.warning.wrongPasswordMsg")
+    public WebElement wrongErrorMessage;
+     
+
+
+
+}
